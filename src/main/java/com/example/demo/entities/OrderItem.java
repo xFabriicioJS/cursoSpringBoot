2b9e7348815eee0942975a0908f2722dc1ba9104
xFabriicioJS <fabriciocostamonteiro@hotmail.com>
2022-07-06 20:44:10 -0300

    Métodos total e subTotal

--- src/main/java/com/example/demo/entities/OrderItem.java
+++ src/main/java/com/example/demo/entities/OrderItem.java
@@ -65,6 +65,10 @@ public class OrderItem implements Serializable{
         this.price = price;
     }
 
+    public Double getSubTotal(){
+        return price * quantity;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -90,5 +94,5 @@ public class OrderItem implements Serializable{
         return true;
     }
     
-    
+
 }
